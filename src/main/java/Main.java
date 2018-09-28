a6066732e9c5082102f9a14587ce12624fd408ab
GosiaBakM <machmalgorzata@gmail.com>
2018-09-28 22:40:31 +0200

    Add all method to sorting (#4)
     *implementation method in SortingClass
     *displaying results in terminal

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,16 @@ public class Main {
 
     public static void main(String[] args) {
 
+//        for (int i:  new RandomArrayGenerator(20).getRandomTable()) {
+//           System.out.println(i);
+//        }
 
+        SortingClass sortingClass = new SortingClass();
 
+        for (int i: sortingClass.sortingByCounting(20)
+             ) {
+            System.out.println(i);
+        }
+
+    }
 }
--- src/main/java/SortingClass.java
+++ src/main/java/SortingClass.java
@@ -1,23 +1,36 @@
 public class SortingClass {
 
-private RandomArrayGenerator table[];
+    private int [] sortedArray;
 
-int[] sortingByCounting(int sizeOfArray){
-  RandomArrayGenerator randomArrayGenerator = new RandomArrayGenerator(sizeOfArray);
-  int [] randomArray = randomArrayGenerator.getRandomTable();
-    int maxValueOfArray = randomArray[0];
+    int[] sortingByCounting(int sizeOfArray) {
+        RandomArrayGenerator randomArrayGenerator = new RandomArrayGenerator(sizeOfArray);
+        int[] randomArray = randomArrayGenerator.getRandomTable();
+        int maxValueOfArray = randomArray[0];
 
-    for (int i = 0; i < (sizeOfArray -1); i++) {
-        if(randomArray[i+1] > randomArray[i] ){
-            maxValueOfArray = randomArray[i+1];
+        for (int i = 0; i < (sizeOfArray - 1); i++) {
+            if (maxValueOfArray < randomArray[i + 1]) {
+                maxValueOfArray = randomArray[i + 1];
+            }
         }
+        int[] replacementArray = new int[maxValueOfArray + 1];
+
+        for (int i = 0; i < (sizeOfArray); i++) {
+            if (replacementArray[randomArray[i]] == 0)
+                replacementArray[randomArray[i]] = 1;
+            else replacementArray[randomArray[i]] ++ ;
+        }
+        int indexOfSortedArray = 0;
+        int [] sortedArray = new int [sizeOfArray];
+
+        for (int i = 1; i < replacementArray.length; i++) {
+            if(replacementArray[i] == 0 ) continue;
+            for (int j = 0; j < replacementArray[i]; j++) {
+                sortedArray[indexOfSortedArray] = i;
+                indexOfSortedArray++;
+            }
+        }
+        return sortedArray;
     }
-    int [] replacementArray = new int[maxValueOfArray];
-
-
-return null;
-}
-
 }
 
 
